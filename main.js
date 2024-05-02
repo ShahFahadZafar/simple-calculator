4ea8a21f96eacd011eb78e11a1cd99214bb12f44
ShahFahadZafar <zafarshahfahad@gmail.com>
2024-05-01 23:57:31 -0700

    little improve logic

--- main.js
+++ main.js
@@ -1,47 +1,70 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 import chalk from "chalk";
-const answer = await inquirer.prompt([
-    {
-        type: "number",
-        name: "firstNumber",
-        message: "Enter the frist number"
-    },
-    {
-        type: "number",
-        name: "secondNumber",
-        message: "Enter the second number"
-    },
-    {
-        type: "list",
-        name: "operetor",
-        message: "choose an operetor",
-        choices: ["+", "-", "*", "/", "**", "%"],
-    },
-]);
-if (answer.operetor === "+") {
-    console.log(answer.firstNumber + answer.secondNumber);
-}
-else if (answer.operetor === "-") {
-    console.log(answer.firstNumber - answer.secondNumber);
-}
-else if (answer.operetor === "*") {
-    console.log(answer.firstNumber * answer.secondNumber);
-}
-else if (answer.operetor === "**") {
-    console.log(answer.firstNumber ** answer.secondNumber);
-}
-else if (answer.operetor === "%") {
-    console.log(answer.firstNumber % answer.secondNumber);
-}
-else if (answer.operetor === "/") {
-    if (answer.secondNumber === 0) {
-        console.log(chalk.red.bold("cannot divide by zero. please enter a non-zero number..."));
+while (true) {
+    console.log(chalk.bold.cyanBright("\t\t Welcome to the Calculator"));
+    console.log(chalk.bold.cyanBright("---------------------------------------------------------------------------"));
+    const answer = await inquirer.prompt([
+        {
+            type: "number",
+            name: "firstNumber",
+            message: "Enter the frist number:",
+            validate: (input) => {
+                if (isNaN(parseFloat(input))) {
+                    return "Please enter a valid number";
+                }
+                return true;
+            },
+        },
+        {
+            type: "number",
+            name: "secondNumber",
+            message: "Enter the second number:",
+            validate: (input) => {
+                if (isNaN(parseFloat(input))) {
+                    return "Please enter a valid number";
+                }
+                return true;
+            },
+        },
+        {
+            type: "list",
+            name: "operetor",
+            message: "choose an operetor:",
+            choices: ["+", "-", "*", "/", "**", "%"],
+        },
+    ]);
+    const { firstNumber, secondNumber, operetor } = answer;
+    if (operetor === "+") {
+        console.log(chalk.bold.greenBright(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`));
+        console.log(chalk.bold.greenBright("---------------------------------------------------------------------------"));
     }
-    console.log(answer.firstNumber / answer.secondNumber);
+    else if (operetor === "-") {
+        console.log(chalk.bold.greenBright(`${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}`));
+        console.log(chalk.bold.greenBright("---------------------------------------------------------------------------"));
+    }
+    else if (operetor === "*") {
+        console.log(chalk.bold.greenBright(`${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`));
+        console.log(chalk.bold.greenBright("---------------------------------------------------------------------------"));
+    }
+    else if (operetor === "/") {
+        if (secondNumber === 0) {
+            console.log(chalk.bold.redBright("\n Error: Cannot divide by zero"));
+            console.log(chalk.bold.redBright("---------------------------------------------------------------------------"));
+        }
+        else
+            console.log(chalk.bold.greenBright(`${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`));
+    }
+    else if (operetor === "**") {
+        console.log(chalk.bold.greenBright(`${firstNumber} ** ${secondNumber} = ${firstNumber ** secondNumber}`));
+        console.log(chalk.bold.greenBright("---------------------------------------------------------------------------"));
+    }
+    else if (operetor === "%") {
+        console.log(chalk.bold.greenBright(`${firstNumber} % ${secondNumber} = ${firstNumber % secondNumber}`));
+        console.log(chalk.bold.greenBright("---------------------------------------------------------------------------"));
+    }
+    else {
+        console.log(chalk.bold.redBright("Invalid operetor"));
+    }
+    ;
 }
-else {
-    console.log("Error: Invalid operetor");
-}
-;
-console.log("Thanks for using this simple calculator");
--- main.ts
+++ main.ts
@@ -3,46 +3,123 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
 
+while (true) {
+
+console.log(chalk.bold.cyanBright("\t\t Welcome to the Calculator"));
+console.log(
+  chalk.bold.cyanBright(
+    "---------------------------------------------------------------------------"
+  )
+);
 
 const answer = await inquirer.prompt([
-    {
-        type: "number",
-        name: "firstNumber",
-        message: "Enter the frist number"
+  {
+    type: "number",
+    name: "firstNumber",
+    message: "Enter the frist number:",
+    validate: (input) => {
+      if (isNaN(parseFloat(input))) {
+        return "Please enter a valid number";
+      }
+      return true;
     },
-    {
-        type: "number",
-        name: "secondNumber",
-        message: "Enter the second number"
+  },
+
+  {
+    type: "number",
+    name: "secondNumber",
+    message: "Enter the second number:",
+    validate: (input) => {
+      if (isNaN(parseFloat(input))) {
+        return "Please enter a valid number";
+      }
+      return true;
     },
-    {
-        type: "list",
-        name: "operetor",
-        message: "choose an operetor",
-        choices: ["+", "-", "*", "/","**", "%"],
-    },
+  },
+
+  {
+    type: "list",
+    name: "operetor",
+    message: "choose an operetor:",
+    choices: ["+", "-", "*", "/", "**", "%"],
+  },
 ]);
 
-if (answer.operetor === "+") {
-    console.log(answer.firstNumber + answer.secondNumber);
-}else if (answer.operetor === "-") {
-    console.log(answer.firstNumber - answer.secondNumber);
-} else if (answer.operetor === "*") {
-    console.log(answer.firstNumber * answer.secondNumber);
-} else if (answer.operetor === "**") {
-    console.log(answer.firstNumber ** answer.secondNumber);
-} else if (answer.operetor === "%") {
-    console.log(answer.firstNumber % answer.secondNumber);
- 
-} else if (answer.operetor === "/") {
-    if (answer.secondNumber === 0){
-        console.log(chalk.red.bold("cannot divide by zero. please enter a non-zero number..."));
-    }
-    console.log(answer.firstNumber / answer.secondNumber);
+const { firstNumber, secondNumber, operetor } = answer;
+
+if (operetor === "+") {
+  console.log(
+    chalk.bold.greenBright(
+      `${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`
+    )
+  );
+  console.log(
+    chalk.bold.greenBright(
+      "---------------------------------------------------------------------------"
+    )
+  );
+} else if (operetor === "-") {
+  console.log(
+    chalk.bold.greenBright(
+      `${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}`
+    )
+  );
+  console.log(
+    chalk.bold.greenBright(
+      "---------------------------------------------------------------------------"
+    )
+  );
+} else if (operetor === "*") {
+  console.log(
+    chalk.bold.greenBright(
+      `${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`
+    )
+  );
+  console.log(
+    chalk.bold.greenBright(
+      "---------------------------------------------------------------------------"
+    )
+  );
+} else if (operetor === "/") {
+  if (secondNumber === 0) {
+    console.log(chalk.bold.redBright("\n Error: Cannot divide by zero"));
+    console.log(
+      chalk.bold.redBright(
+        "---------------------------------------------------------------------------"
+      )
+    );
+
+  } else
+    console.log(
+      chalk.bold.greenBright(
+        `${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`
+      )
+    );
+    
+} else if (operetor === "**") {
+  console.log(
+    chalk.bold.greenBright(
+      `${firstNumber} ** ${secondNumber} = ${firstNumber ** secondNumber}`
+    )
+  );
+    console.log(
+      chalk.bold.greenBright(
+        "---------------------------------------------------------------------------"
+      )
+    );
+} else if (operetor === "%") {
+  console.log(
+    chalk.bold.greenBright(
+      `${firstNumber} % ${secondNumber} = ${firstNumber % secondNumber}`
+    )
+  );
+    console.log(
+      chalk.bold.greenBright(
+        "---------------------------------------------------------------------------"
+      )
+    );
 } else {
-    console.log("Error: Invalid operetor");
+  console.log(chalk.bold.redBright("Invalid operetor"));
 };
-
-console.log("Thanks for using this simple calculator")
-
+}
 
